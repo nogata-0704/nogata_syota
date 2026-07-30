5cda6ffceda2345dc673e31ded6553c1ef2d9052
nogata <nogata_syota>
2026-07-30 13:35:06 +0900

    Task11修正版

--- 20260529/Task11.cpp
+++ 20260529/Task11.cpp
@@ -6,27 +6,24 @@ int main()
   try
   {
     const double price = 1500.0;   // 定価（税抜）
-        const double taxRate = 0.10;   // 消費税率（10%）
+    const double taxRate = 0.10;   // 消費税率（10%）
 
-     // 消費税額を計算（四捨五入）
+    // 消費税額を計算（四捨五入）
     double tax = price * taxRate;
     tax = std::round(tax); // 円単位に丸める
 
     // 合計金額
-     double total = price + tax;
+    double total = price + tax;
 
-     //出力
-     std::cout <<std::fixed << std::setprecision(0);//小数点なし
-     std::cout <<"定価:" << price << "円:";
-     std::cout <<"消費税:" << tax << "円:";
-     std::cout <<"合計金額:" << total << "円:";
-
-  }
-  catch(const std::exception& e)
-  {
-    std::cerr << "エラー" <<e.what() << '\n';
+    // 出力
+    std::cout << std::fixed << std::setprecision(0); // 小数点なし
+    std::cout << "定価: " << price << "円\n";
+    std::cout << "消費税: " << tax << "円\n";
+    std::cout << "合計金額: " << total << "円\n";
+    }
+     catch (const std::exception &e) {
+     std::cerr << "エラー: " << e.what() << '\n';
     return 1;
-  }
-  return 0;
-  
+    }
+      return 0;
 }
